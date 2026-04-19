4e2332d74cac6d94209835e32a16214441459359
dominickgzc-hash <dominickgzc@gmail.com>
2026-04-18 19:07:57 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Gracias por tu mensaje, nos pondremos en contacto pronto.');
+function showSection(sectionId) {
+    // Ocultar todas las secciones
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    
+    // Mostrar la sección seleccionada
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
+
+// Mostrar la sección de inicio por defecto al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    showSection('inicio');
 });
