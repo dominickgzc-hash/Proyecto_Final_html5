f0229254ecf8bcbf162e09ac3f13a2248da1f14e
dominickgzc-hash <dominickgzc@gmail.com>
2026-04-18 18:58:13 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por tu mensaje, nos pondremos en contacto pronto.');
+});
